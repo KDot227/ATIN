86d5bdab19d90ff6df9bf087b1d75f9d5fc18d25
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-08-30 09:24:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+def main():
+    file = input("What is path of file? -> ") # Path of file to tif
+    with open(f"{file}") as f: #open the file person put in path
+        try: #try to get rid of file extension for the name
+            file, lel = file.split('.')
+        except: #uh oh
+            pass #ignore
+        for line in f: #lines
+           for character in line: #characters
+                tif = '​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​'
+                with open(f"{file}TIF.py", "a", encoding="utf-8") as f: #open the file and prite in that shi
+                    f.write(f"{character}{tif}") #write the character and then tif
+if __name__ == "__main__":
+    main()
