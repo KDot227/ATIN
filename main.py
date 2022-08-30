aded92ddee587b5b8dfcc7859c2b873eb1abd887
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-08-30 09:40:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     file = input("What is path of file? -> ") # Path of file to tif
-    with open(f"{file}") as f: #open the file person put in path
+    with open(f"{file}", encoding='utf-8', errors='ignore') as f: #open the file person put in path
         try: #try to get rid of file extension for the name
             file, lel = file.split('.')
         except: #uh oh
@@ -8,7 +8,7 @@ def main():
         for line in f: #lines
            for character in line: #characters
                 tif = '​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​'#opposite of timmywag
-                with open(f"{file}TIF.py", "a", encoding="utf-8") as f: #open the file and write the stuffs
+                with open(f"{file}TIF.py", "a", encoding="utf-8", errors='ignore') as f: #open the file and write the stuffs
                     f.write(f"{character}{tif}") #write the character and then tif
 if __name__ == "__main__": #ngl I don't know what this does
     main() #whole ahh function
